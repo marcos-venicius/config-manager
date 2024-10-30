ddb9115b65aedc5a73a6c894819ef34bd1c7f781
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-10-30 14:41:02 -0300

    reinstall when update

--- commands/update.go
+++ commands/update.go
@@ -15,7 +15,7 @@ func Update() error {
 
 	appLocation, err := utils.GetEnv(utils.APP_LOCATION_ENV_NAME)
 
-	stdout, stderr, err := utils.Exec(fmt.Sprintf("cd \"%s\" && git fetch && git fetch --prune && git pull", appLocation))
+	stdout, stderr, err := utils.Exec(fmt.Sprintf("cd \"%s\" && git fetch && git fetch --prune && git pull && go install", appLocation))
 
 	if err != nil {
 		fmt.Printf(stderr)
