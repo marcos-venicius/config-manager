ee1cfda42f15faa01f1ac4fbc9b9ee5f5cac4b55
Marcos Venicius <marcosdevelopment.business@gmail.com>
2025-05-07 09:42:28 -0300

    remove println

--- commands/update.go
+++ commands/update.go
@@ -12,7 +12,6 @@ func Update() error {
 	if !utils.PathExists(appLocation, true) {
 		return fmt.Errorf("\"%s\" is not a valid folder location", appLocation)
 	}
-	println(appLocation)
 
 	stdout, stderr, err := utils.Exec(fmt.Sprintf("cd \"%s\" && git fetch && git fetch --prune && git pull && go install", appLocation))
 
