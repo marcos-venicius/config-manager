4beef1df13e23630401f5caa7b3402e4914326ff
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-04-20 09:18:13 -0300

    feat(nvim): goto next and prev hunk

--- configs/nvim/lua/plugins/git.lua
+++ configs/nvim/lua/plugins/git.lua
@@ -2,9 +2,23 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup {
+      local gs = require('gitsigns')
+
+      gs.setup {
         current_line_blame = true
       }
+
+      vim.keymap.set('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
+        return '<Ignore>'
+      end, {expr=true, desc = "Jump to next hunk"})
+
+      vim.keymap.set('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
+        return '<Ignore>'
+      end, {expr=true, desc = "Jump to previous hunk"})
     end
   }
 }
